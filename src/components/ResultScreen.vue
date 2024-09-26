f8d44ab9d393c2220e2febc3fbc89f25e7424903
Kguinesh <khushali.guinesh@gmail.com>
2024-09-26 21:15:57 +0200

    Updated ResultScreen.vue (Added the Try Again Button)

--- src/components/ResultScreen.vue
+++ src/components/ResultScreen.vue
@@ -7,7 +7,8 @@
         <p>
           You got <b>score</b> correct answers.</p>
       </div>
-      
+
+      <button class="result-box__restart-button">Try Again</button>
     </div>
   </template>
   
@@ -43,6 +44,24 @@
       text-align: center;
       color: blue;
       font-size: 18px; 
+    }
+
+    
+  
+    .result-box__restart-button {
+      background-color: white;
+      color: blue;
+      padding: 15px 40px;
+      border-color: blue;
+      border-radius: 12px;
+      cursor: pointer;
+      font-size: 16px;
+      transition: background-color 0.3s ease;
+    }
+      
+    .result-box__restart-button:hover {
+        transform: translateY(-2px);
+        box-shadow: 0 1rem 2rem rgba(29, 53, 93, 0.2) ;
     }
   
 
